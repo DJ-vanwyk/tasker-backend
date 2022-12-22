b879b063d8872fd2c7c9362f621ed0acf7323bdd
DJ-V2Tech <djvanwyk100@gmail.com>
2022-12-22 09:07:16 +0200

    Updated Login controler to accept json body

--- app/controllers/auth/login.controller.php
+++ app/controllers/auth/login.controller.php
@@ -4,13 +4,23 @@ class LoginController extends Controller
 {
     public function go()
     {
+        $body = json_decode($this->request->body, true);
+
+        if (!isset($body['username']) or !isset($body['password'])) {
+            http_response_code(400);
+            echo json_encode(['error' => 'Missing username or password']);
+            exit();
+        }
+
         $model = new UsersModel();
-        $user = $model->get_user_by_username($_POST['username']);
+        $user = $model->get_user_by_username($body['username']);
 
         if ($user) {
-            if (password_verify($_POST['password'], $user['password'])) {
+            if (password_verify($body['password'], $user['password'])) {
                 $_SESSION['user'] = $user;
-                echo 'Logged in';
+                unset($user['password']);
+                header('Content-Type: application/json');
+                echo json_encode($user);
             } else {
                 echo 'Wrong password';
             }
