c0360d6a8306032cfaf980088987b781c3cda92e
dj-vanwyk <djvanwyk100@gmail.com>
2022-12-23 23:22:20 +0200

    Updated get Status resp

--- app/controllers/statuses/getStatuses.controller.php
+++ app/controllers/statuses/getStatuses.controller.php
@@ -5,10 +5,9 @@ class GetStatusesController extends Controller
     public function go()
     {
         $model = new StatusesModel();
-        $statuse = $model->get_all_statuses();
+        $statuses = $model->get_all_statuses();
 
-        // Set the header to JSON
-        header('Content-Type: application/json');
-        echo json_encode($statuse);
+        $response = new Response(200, "Ok", $statuses);
+        $response->send_json();
     }
 }
