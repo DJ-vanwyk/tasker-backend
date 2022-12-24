0545e46055774f4a4e5c5be47b1b73d61c973812
dj-vanwyk <djvanwyk100@gmail.com>
2022-12-24 12:43:59 +0200

    Updated CORS to accept put and del http methods

--- app/scripts/cors.php
+++ app/scripts/cors.php
@@ -15,7 +15,7 @@ function cors()
     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
             // may also be using PUT, PATCH, HEAD etc
-            header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+            header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
         }
 
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
