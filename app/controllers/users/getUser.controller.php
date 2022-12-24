1d82f6ec453bbdace2d3ec9c73fa617f99145f83
dj-vanwyk <djvanwyk100@gmail.com>
2022-12-24 23:15:40 +0200

    Updated get user response

--- app/controllers/users/getUser.controller.php
+++ app/controllers/users/getUser.controller.php
@@ -5,6 +5,7 @@ class GetUserController extends Controller {
     public function go() {
         $model = new UsersModel();
         $user = $model->get_user_by_id($this->request->path_segments[count($this->request->path_segments) - 1]);
+        unset($user['password']);
         $response = new Response(200,"OK", $user);
         $response->send_json();
     }
