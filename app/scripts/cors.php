cb3c13c67f6d685c5d8f9c444f41322d33c3b0a3
dj-vanwyk <djvanwyk100@gmail.com>
2022-12-24 12:44:24 +0200

    Added Del method to CORS script

--- app/scripts/cors.php
+++ app/scripts/cors.php
@@ -15,7 +15,7 @@ function cors()
     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
             // may also be using PUT, PATCH, HEAD etc
-            header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
+            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
         }
 
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
